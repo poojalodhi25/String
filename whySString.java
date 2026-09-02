91231254ef6cb201d16d273a04fde7be1906c985
Pooja Lodhi <lodhipooja952@gmail.com>
2026-09-02 18:12:30 +0530

    Add string programs

---
+++ whySString.java
@@ -0,0 +1,7 @@
+public class whySString{
+	public static void main(String[] args){
+	  char [] arr={'p', 'o', 'o','j','a'};
+	  for(int i=0; i<arr.length; i++)
+	      System.out.println(arr[i]);
+	}
+}	
